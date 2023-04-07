6c93fa759f3492bff0079bce7ce231e7c270bb9f
Christian <chriscar.sam@gmail.com>
2023-04-07 12:27:42 -0500

    Persistiendo una entidad #1

---
+++ src/main/java/org/sam/alura/tienda/prueba/RegistroDeProducto.java
@@ -0,0 +1,24 @@
+package org.sam.alura.tienda.prueba;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.sam.alura.tienda.modelo.Producto;
+
+public class RegistroDeProducto {
+	public static void main(String[] args) {
+		Producto celular = new Producto();
+		celular.setNombre("Samsung");
+		celular.setDescripcion("teléfono usado");
+		celular.setPrecio(new BigDecimal("1000"));
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tienda");
+		EntityManager em = factory.createEntityManager();
+		
+		em.persist(celular);
+		
+	}
+}

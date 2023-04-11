a51ecf83d6320e4e73e23b52a687500459795301
Christian <chriscar.sam@gmail.com>
2023-04-11 10:52:10 -0500

    Test de relacionamiento bidireccional #1

---
+++ src/main/java/org/sam/alura/tienda/prueba/RegistroDePedido.java
@@ -0,0 +1,58 @@
+package org.sam.alura.tienda.prueba;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.sam.alura.tienda.dao.CategoriaDao;
+import org.sam.alura.tienda.dao.PedidoDao;
+import org.sam.alura.tienda.dao.ProductoDao;
+import org.sam.alura.tienda.modelo.Categoria;
+import org.sam.alura.tienda.modelo.Cliente;
+import org.sam.alura.tienda.modelo.ItemsPedido;
+import org.sam.alura.tienda.modelo.Pedido;
+import org.sam.alura.tienda.modelo.Producto;
+import org.sam.alura.tienda.utils.JPAUtils;
+
+public class RegistroDePedido {
+	public static void main(String[] args) {
+		registrarProducto();
+		
+		EntityManager em = JPAUtils.getEntityManager();
+		
+		ProductoDao productoDao = new ProductoDao(em);
+		Producto producto = productoDao.consultaPorId(1l);
+		
+		PedidoDao pedidoDao = new PedidoDao(em);
+		
+		Cliente cliente = new Cliente("Juan", "k675kjb");
+		Pedido pedido = new Pedido(cliente);
+		pedido.agregarItems(new ItemsPedido(5, producto, pedido));	
+		
+		em.getTransaction().begin();	
+		
+		pedidoDao.guardar(pedido);
+		
+		em.getTransaction().commit();
+		 
+	}
+
+	private static void registrarProducto() {
+		Categoria celulares = new Categoria("CELULARES");
+
+		Producto celular = new Producto("Xiaomi Redmi", "Muy bueno", new BigDecimal("800"), celulares);
+
+	    EntityManager em = JPAUtils.getEntityManager();
+	    ProductoDao productoDao = new ProductoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+        
+	    em.getTransaction().begin();
+	    
+	    categoriaDao.guardar(celulares);
+	    productoDao.guardar(celular);	
+	    
+	    em.getTransaction().commit();
+	    em.close();
+	}
+}

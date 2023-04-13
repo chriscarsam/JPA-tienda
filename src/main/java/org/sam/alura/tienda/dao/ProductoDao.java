d83730b4d8a1c92fd7df2bebec8b909d3a83277b
Christian <chriscar.sam@gmail.com>
2023-04-13 14:05:12 -0500

    Consultas con Criteria API #1

--- src/main/java/org/sam/alura/tienda/dao/ProductoDao.java
+++ src/main/java/org/sam/alura/tienda/dao/ProductoDao.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Predicate;
 
 import org.sam.alura.tienda.modelo.Producto;
 
@@ -79,5 +83,25 @@ public class ProductoDao {
 		
 		return query.getResultList();
 		
+	}
+	
+	public List<Producto> consultarPorParametrosConAPICriteria(String nombre, BigDecimal precio, LocalDate fecha) {
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Producto> query = builder.createQuery(Producto.class);
+		Root<Producto> from = query.from(Producto.class);
+		
+		Predicate filtro = builder.and();
+		if (nombre!=null && !nombre.trim().isEmpty()) {
+			filtro=builder.and(filtro, builder.equal(from.get("nombre"), nombre));
+		}
+		if (precio!=null && !precio.equals(new BigDecimal(0))) {
+			filtro=builder.and(filtro, builder.equal(from.get("precio"), precio));
+		}
+		if (fecha!=null) {
+			filtro=builder.and(filtro, builder.equal(from.get("fechaDeRegistro"), fecha));
+		}
+		
+		query = query.where(filtro);
+		return em.createQuery(query).getResultList();
 	}
 }
